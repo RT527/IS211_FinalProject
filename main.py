cec58d5af9e1d5009cde4ca793f2ff8fe962d59f
“RT527” <rafi.talukder7@gmail.com>
2025-12-08 22:05:46 -0500

    add html

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 # Rafi Talukder Final Project
+"""---------------------------------------------------------------------------------"""
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
